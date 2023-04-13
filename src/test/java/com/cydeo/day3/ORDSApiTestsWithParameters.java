d193f095ec127d14016d422b8b2caf08ef7c5455
Sumeyra <smyryalcin@gmail.com>
2023-04-13 17:05:11 +0100

    day3 created

--- src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
@@ -14,7 +14,7 @@ public class ORDSApiTestsWithParameters {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://3.216.30.92:1000/ords/hr";
+        baseURI = "http://54.161.232.48:1000/ords/hr";
     }
 
     /*
