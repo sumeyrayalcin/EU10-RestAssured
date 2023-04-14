d8dd1b5b170693a308c2300f222f5c13b89278d6
Sumeyra <smyryalcin@gmail.com>
2023-04-14 11:10:39 +0100

    day3 spartans

--- src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -18,7 +18,7 @@ public class SpartanTestsWithParameters {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://3.216.30.92:8000";
+        baseURI = "http://54.161.232.48:8000";
     }
 
      /*   Given accept type is Json
