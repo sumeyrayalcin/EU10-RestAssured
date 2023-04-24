f40302365f665e8334bb52b550a2a546c1d45aa7
Sumeyra <smyryalcin@gmail.com>
2023-04-24 11:17:39 +0100

    day8 bookit test class updated

--- src/test/java/com/cydeo/day8/BookItTest.java
+++ src/test/java/com/cydeo/day8/BookItTest.java
@@ -1,4 +1,47 @@
 package com.cydeo.day8;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import io.restassured.http.*;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.*;
+
 public class BookItTest {
+    /*
+    email: emaynell8f@google.es
+password: besslebond
+link: http://cybertek-reservation-api-qa3.herokuapp.com/sign
+     */
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://cybertek-reservation-api-qa3.herokuapp.com";
+
+    }
+
+    //create BookItUtil then create a method, that accepts email and password return token Bearer +yourToken as a String
+
+    String accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4NiIsImF1ZCI6InN0dWRlbnQtdGVhbS1sZWFkZXIifQ.lEfjcu6RpBfcZ4qWthzZU8uH8fX4FCJFfxBnPNgh4Mo";
+
+    @DisplayName("GET all campuses")
+    @Test
+    public void testAuth1(){
+        //how to pass bearer token for bookit ? use header method to give as key value header
+        given().
+                header("Authorization",accessToken)
+                .and().accept(ContentType.JSON)
+                .when()
+                .get("/api/campuses")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+
 }
